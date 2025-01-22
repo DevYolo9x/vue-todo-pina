ef2bfa3849d9673cee7441e6fee6c18e82e5eee1
BinhTx <xuanbinh1306@gmail.com>
2025-01-22 16:49:03 +0700

    chia component va tao du lieu

---
+++ components/ModalWindow.vue
@@ -0,0 +1,103 @@
+<template lang="">
+    <div class="modal-wrapper" aria-modal="true"
+      role="dialog" tabindex="-1">
+      <div class="inner">
+           <div class="form">
+                <ModalCloseButtonVue />
+                <div class="add-task">
+                    <h3>Add a new task</h3>
+                    <input v-model="newTask.name" type="text" name="title" placeholder="Enter a title..."><br />
+                    <textarea v-model="newTask.description" name="description" rows="4" placeholder="Enter a description..." /><br />
+                    <button @click="addTask" class="btn gray">Add Task</button>
+                </div>
+           </div>
+      </div>
+  </div>
+</template>
+<script setup>
+import ModalCloseButtonVue from "./ModalCloseButton.vue";
+const props = defineProps(["newTask"]);
+</script>
+<style lang="scss" scoped>
+    .modal-wrapper {
+       position: fixed;
+       left: 0;
+       top: 0;
+       z-index: 500;
+       width: 100vw;
+       height: 100vh;
+       background: rgba(0, 0, 0, 0.2);
+       display: grid;
+       place-items: center;
+       color: #000;
+  
+       .inner {
+       background-color: white;
+       padding: 30px;
+       border-radius: 12px;
+       display: flex;
+       flex-direction: column;
+       position: relative;
+       max-width: 600px;
+       width: 90%;
+  
+       h3 {
+           font-size: 16px;
+           font-weight: 700;
+           line-height: 21px;
+           margin-bottom: 20px;
+       }
+  
+       .close-btn {
+           position: absolute;
+           top: 15px;
+           right: 15px;
+           cursor: pointer;
+           background-color: #fff;
+       }
+  
+       .form {
+           display: flex;
+           flex-direction: column;
+           max-width: 100%;
+
+
+           label {
+           font-size: 12px;
+           font-weight: 500;
+           line-height: 16px;
+           letter-spacing: 0em;
+           text-align: left;
+           }
+
+
+           input,
+           select,
+           textarea {
+           font-size: 12px;
+           font-weight: 400;
+           line-height: 16px;
+           letter-spacing: 0em;
+           text-align: left;
+           border: 1px solid #C2C2C2;
+           border-radius: 4px;
+           padding: 8px 12px;
+           margin-top: 5px;
+
+
+           &::placeholder {
+               color: #A6A6A6;
+           }
+           }
+  
+          
+  
+           .btn {
+           width: fit-content;
+           padding-inline: 23px;
+           }
+       }
+       }
+   }
+
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,95 @@
 <script setup>
 
-const tasks= [
+import { ref, reactive, computed } from 'vue';
+
+import Task from '../components/Task.vue';
+import Filter from '../components/Filter.vue';
+import ModalWindow from '../components/ModalWindow.vue';
+
+const appName = ref('Task mannager')
+
+let filterBy = ref('')
+
+let modalIsActivated = ref(false);
+
+const filteredTasks = computed( () => {
+  if( filterBy.value === 'todo' ) {
+    return tasks.filter( task => !task.completed );
+  } else if( filterBy.value === 'done' ) {
+    return tasks.filter( task => task.completed );
+  } else {
+    return tasks;
+  }
+})
+
+const tasks= reactive([
     {
       name: "Website design",
       description: "Define the style guide, branding and create the webdesign on Figma.",
-      completed: true
+      completed: true,
+      id: 1
     },
     {
       name: "Website development",
       description: "Develop the portfolio website using Vue JS.",
-      completed: false
+      completed: false,
+      id: 2
     },
     {
       name: "Hosting and infrastructure",
       description: "Define hosting, domain and infrastructure for the portfolio website.",
-      completed: false
+      completed: false,
+      id: 3
     },
     {
       name: "Composition API",
       description: "Learn how to use the composition API and how it compares to the options API.",
-      completed: true
+      completed: true,
+      id: 4
     },
     {
       name: "Pinia",
       description: "Learn how to setup a store using Pinia.",
-      completed: true
+      completed: true,
+      id: 5
     },
     {
       name: "Groceries",
       description: "Buy rice, apples and potatos.",
-      completed: false
+      completed: false,
+      id: 6
     },
     {
       name: "Bank account",
       description: "Open a bank account for my freelance business.",
-      completed: false
+      completed: false,
+      id: 7
     }
-];
+]);
+
+let newTask = {completed: false}
+
+function addTask() {
+  if( !newTask.name || !newTask.description) {
+    alert('Please fill in all fields');
+    return false;
+  }
+  newTask.id = tasks.length;
+  tasks.push(newTask);
+  newTask = {completed: false}
+}
+
+function toggleCompleted(id) {
+  const task = tasks.map( task => {
+    if( task.id == id ) {
+      task.completed = !task.completed;
+    }
+  })
+}
+
+function setFilter(filter) {
+  filterBy.value = filter;
+}
 
 </script>
 
@@ -46,69 +99,18 @@ const tasks= [
     <div class="header">
       <div class="header-side">
         <h1>
-          Tasks Manager
+          {{ appName }}
         </h1>
       </div>
     </div>
     
-    <div class="filters">
-      <div>
-        <p>Filter by state</p>
-        <div class="badges">
-          <div class="badge">
-            To-Do
-          </div>
-          <div class="badge">
-            Done
-          </div>
-          <span class="clear">
-            x clear
-          </span>
-        </div>
-      </div>
-    </div>
+    <Filter @setFilter="setFilter" :filterBy="filterBy" />
 
     <div class="tasks">
-      
-      <div class="task">
-        <h3>
-          Website design
-        </h3>
-        <p>
-          Define the style guide, branding and create the webdesign on Figma.
-        </p>
-        <div class="task-check">
-          <input type="checkbox" checked />
-          <label>
-            Done
-          </label>
-        </div>
-      </div>
-
-      <div class="task">
-        <h3>
-          Website development
-        </h3>
-        <p>
-          Develop the portfolio website using Vue JS.
-        </p>
-        <div class="task-check">
-          <input type="checkbox"/>
-          <label>
-            To-Do
-          </label>
-        </div>
-      </div>
-
+      <Task @toggleCompleted="toggleCompleted" v-for="task in filteredTasks" :key="task.id" :task="task" />
     </div>
 
-    <div class="add-task">
-      <h3>Add a new task</h3>
-      <input type="text" name="title" placeholder="Enter a title..."><br />
-      <textarea name="description" rows="4" placeholder="Enter a description..." /><br />
-      <button class="btn gray">Add Task</button>
-
-    </div>
+    <ModalWindow v-if="modalIsActivated" :newTask="newTask" />
 
   </main>
   
@@ -144,38 +146,6 @@ const tasks= [
 
 }
 
-.filters {
-  display: flex;
-  flex-direction: column;
-  margin: 40px 0;
-
-  p {
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 21px;
-    letter-spacing: 0em;
-    text-align: left;
-  }
-
-  .badges {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 12px;
-    margin: 14px 0;
-    align-items: center;
-  }
-
-  .clear {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 16px;
-    letter-spacing: 0em;
-    text-align: left;
-    cursor: pointer;
-  }
-
-}
-
 .tasks {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
@@ -186,82 +156,7 @@ const tasks= [
   }
 }
 
-.task {
-  display: flex;
-  flex-direction: column;
-  background-color: var(--white-color);
-  color: var(--black-color);
-  padding: 20px;
-  border-radius: 12px;
-  position: relative;
 
-
-  h3 {
-    font-size: 20px;
-    font-weight: 700;
-    line-height: 21px;
-    letter-spacing: 0em;
-    text-align: left;
-  }
-
-  p {
-    margin-top: 24px;
-    margin-bottom: 12px;
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 16px;
-    letter-spacing: 0em;
-    text-align: left;
-  }
-
-
-  .task-check {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    bottom: 10px;
-    right: 10px;
-
-    label {
-      font-size: 13px;
-      font-weight: 400;
-      line-height: 16px;
-      letter-spacing: 0em;
-      text-align: left;
-      margin-left: 5px;
-      cursor: pointer;
-    }
-
-    input {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 18px;
-      height: 18px;
-      border-radius: 100%;
-      border: 0.77px solid #AEAEB2;
-      appearance: none;
-      cursor: pointer;
-
-
-      &:checked {
-        background-color: #0A7AFF;
-        border-color: #0A7AFF;
-
-        &::before {
-          content: '';
-          display: block;
-          width: 4.5px;
-          height: 9px;
-          border: solid white;
-          border-width: 0 2px 2px 0;
-          transform: rotate(45deg);
-        }
-      }
-    }
-  }
-}
 
 .add-task {
   margin-top: 60px;
